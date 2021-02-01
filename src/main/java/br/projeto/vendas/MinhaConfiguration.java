a29695e71508e8518569917f597aa044c59694ec
Filipe Alves <filipe.alves@sefin.fortaleza.ce.gov.br>
2021-02-01 17:06:23 -0300

    Acrescentando profiles e múltiplos ambientes

--- src/main/java/br/projeto/vendas/MinhaConfiguration.java
+++ src/main/java/br/projeto/vendas/MinhaConfiguration.java
@@ -1,8 +1,18 @@
 package br.projeto.vendas;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class MinhaConfiguration {
 
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("Rodando a configuração de desenvolvimento...");
+		};
+	}
 }
