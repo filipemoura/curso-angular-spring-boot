82a8f995b57ab86780ac951d7328a9041f260592
Filipe Alves <filipe.alves@sefin.fortaleza.ce.gov.br>
2021-02-01 18:39:07 -0300

    Criando @annotations

--- src/main/java/br/projeto/vendas/MinhaConfiguration.java
+++ src/main/java/br/projeto/vendas/MinhaConfiguration.java
@@ -2,11 +2,8 @@ package br.projeto.vendas;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("dev")
+@Development
 public class MinhaConfiguration {
 
 	@Bean
